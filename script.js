0d4b2d17a1d24869195c2efd4736852c2598be73
Emmanuel Orji <emmanuelorji2013@gmail.com>
2021-07-19 20:51:44 +0100

    Initial Commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function formProcess() {
+    fullName = document.getElementById("name").value
+    email = document.getElementById("email").value
+    phone = document.getElementById("phone").value
+
+
+    alert("Your name is: " + fullName +
+            "\nYour email is: " + email +
+            "\n Your Phone number is: " + phone)
+}
